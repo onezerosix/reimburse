bd96cbf97fa5bc965ed64b63245029532e959d6e
Sahar <onezerosix@users.noreply.github>
2024-05-02 18:07:03 -0700

    Add TODO comments for unit testing models

---
+++ backend/tst/test_models.py
@@ -0,0 +1,17 @@
+"""
+TODO:
+
+test default values & limits for all tables
+- account.balance --> 0, max 15 digits, 2 decimal places
+- reimbursement.amount --> 0, max 10 digits, 2 decimal places
+- reimbursement.status --> submitted
+
+test primary key is id and uid are primary and index respectively
+for each table and that uid has default_factory of uuid4
+
+test foreign keys exist:
+- account --> bank_member_id
+- reimbursement --> account_id
+
+test model column types? and enums?
+"""
