bb1ce572f37bd68890ea571a39905bbe5b6b0a0d
Sahar <onezerosix@users.noreply.github>
2024-05-07 13:54:08 -0700

    Update integ test names
    Remove the endpoint name from test names since the whole file is for the
    specific endpoint.

--- backend/tst_integ/test_get_reimbursements.py
+++ backend/tst_integ/test_get_reimbursements.py
@@ -5,10 +5,10 @@ implement tests
 once other tags/prefixes used, group endpoints for each tag under their own folders
 """
 
-def test_get_reimbursements():
+def test_returns_records():
     # TODO: returns the 3 reimbursements set up in db.py
     pass
 
-def test_get_reimbursements_no_records():
+def test_no_records():
     # TODO: (set up db for this test to not interfere with other tests somehow) returns empty list
     pass
